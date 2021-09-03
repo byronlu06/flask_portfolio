63a9ac852783ee1be547a8237464912c5a80874b
chase <chaseott123@gmail.com>
2021-09-02 19:32:19 -0700

    Revert "Commit"
    This reverts commit 7422c6a8bab752d884af01747f274e33b6617c63.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 # create a Flask instance
 app = Flask(__name__)
@@ -30,16 +30,6 @@ def hawkers():
 @app.route('/stub/')
 def stub():
     return render_template("stub.html")
-
-@app.route('/ChaseGreeting/', methods=['GET', 'POST'])
-def ChaseGreeting():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("ChaseGreeting.html", name=name)
-    # starting and empty input default
-    return render_template("ChaseGreeting.html", name="World")
 
 
 # runs the application on the development server
